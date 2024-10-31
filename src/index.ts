71bb92bf53d6af5056884b6c8e22f1a9ad74d831
Alfio Tomarchio <at352@st-andrews.ac.uk>
2024-10-31 17:33:37 +0000

    handle number sign and padding with 0s

--- src/index.ts
+++ src/index.ts
@@ -33,15 +33,15 @@ function format(str: string, ...params: any[]) {
     return str.replace(FORMAT_REGEX,
         (
             $text: string,
-            $param_number: string,
-            $fill_character: string,
-            $align_direction: '^' | '>' | '<',
-            $sign: '+' | '',
-            $pretty: '#' | '',
-            $pad_zeroes: '0' | '',
+            $param_number: string | undefined,
+            $fill_character: string | undefined,
+            $align_direction: '^' | '>' | '<' | undefined,
+            $sign: '+' | undefined,
+            $pretty: '#' | undefined,
+            $pad_zeroes: '0' | undefined,
             $width: string,
             $precision: string,
-            $type: '?' | 'o' | 'x' | 'X' | 'b' | 'e' | 'E' | ''
+            $type: '?' | 'o' | 'x' | 'X' | 'b' | 'e' | 'E' | undefined
         ) => {
             // Return a bracket if the regex matched an escaped bracket
             if ($text === '{{') {
@@ -53,21 +53,17 @@ function format(str: string, ...params: any[]) {
             // Process parameter number; increment param_counter if not included
             let param = $param_number === '' ? params[param_counter++] : params[Number($param_number)];
             if (param === undefined) {
-                throw new Error(`parameter ${$param_number} either NaN or not provided`);
+                throw new Error(`parameter ${$param_number || param_counter - 1} either NaN or not provided`);
             }
+
+            let param_is_number = (typeof param === 'number') || (typeof param === 'bigint');
+
             // Process parameter type
             switch ($type) {
-                case "o":
-                    param = param.toString(8);
-                    break;
-                case "x":
-                    param = param.toString(16);
-                    break;
-                case "X":
-                    param = param.toString(16).toUpperCase();
-                    break;
-                case "b":
-                    param = param.toString(2);
+                case "o": param = param.toString(8); break;
+                case "x": param = param.toString(16); break;
+                case "X": param = param.toString(16).toUpperCase(); break;
+                case "b": param = param.toString(2); break;
                 case "e":
                     switch (typeof param) {
                         case 'number':
@@ -76,7 +72,7 @@ function format(str: string, ...params: any[]) {
                         case 'bigint':
                             param = param.toLocaleString('en-US', {
                                 notation: 'scientific',
-                                maximumFractionDigits: 20
+                                maximumFractionDigits: 20,
                             });
                             break;
                         default:
@@ -101,12 +97,40 @@ function format(str: string, ...params: any[]) {
                     }
                     break;
                 case "?":
-                    param = inspect(param, { depth: Infinity, colors: true, compact: $pretty !== '#' });
+                    param = inspect(param, {
+                        depth: Infinity,
+                        colors: true,
+                        compact: $pretty !== '#'
+                    });
                     break;
-                case '':
-                    param = param.toString();
-                    break;
+                default: param = param.toString(); break;
             };
+
+            let width: number;
+            if ($width === undefined) {
+                width = 0;
+            } else {
+                width = +$width;
+                if (Number.isNaN(width)) throw new Error(`invalid width specifier '${$width}' (must be an integer)`);
+            }
+
+
+            if (param_is_number) {
+                // Compute parameter sign and pad with 0s if specified
+                let maybe_sign = (param as string).substring(0, 1);
+                if (maybe_sign === '-') {
+                    param = (param as string).substring(1, param.length);
+                } else if ($sign === '+') {
+                    maybe_sign = '+';
+                }
+
+                if ($pad_zeroes === '0') {
+                    param = (param as string).padStart(width - maybe_sign.length, "0");
+                }
+
+                param = maybe_sign + param;
+            }
+
             /* TODO */
             return "";
         }
