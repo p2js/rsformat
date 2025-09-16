bb71cbe830806155f441e7d753623add70f6b583
Alfio Tomarchio <at352@st-andrews.ac.uk>
2025-09-16 14:23:48 +0100

    [bump to 0.2.4] Add JSDOC for exported default printing functions

--- src/print.ts
+++ src/print.ts
@@ -2,6 +2,15 @@ import { Writable } from 'node:stream';
 import process from 'node:process';
 import { fmt_raw } from './format';
 
+/**
+ * Create format printer functions with custom output/error streams.
+ * 
+ * @param outStream Output stream (used by print and println - process.stdout by default)
+ * @param errStream Error stream (used by eprint and eprintln - process.stderr by default)
+ * @param options Options for the printer functions (Whether to color the debug formatting in the output - true by default)
+ * 
+ * @returns an object with print, println, eprint and eprintln functions that print to the specified streams
+ */
 export function Printer(outStream: Writable = process.stdout, errStream: Writable = process.stderr, options = { debugColors: true }) {
     return {
         /**
@@ -45,4 +54,34 @@ export function Printer(outStream: Writable = process.stdout, errStream: Writabl
     }
 }
 
-export const { print, println, eprint, eprintln } = Printer();
+const default_printer = Printer();
+/**
+ * Print a format string to process.stdout.
+ * 
+ * @param format_string String used for formatting
+ * @param params Parameters to be inserted into the format string
+ */
+export const print = default_printer.print;
+/**
+ * Print a format string to process.stdout
+ * and append a newline.
+ * 
+ * @param format_string String used for formatting
+ * @param params Parameters to be inserted into the format string
+ */
+export const println = default_printer.println;
+/**
+ * Print a format string to process.stderr.
+ * 
+ * @param format_string String used for formatting
+ * @param params Parameters to be inserted into the format string
+ */
+export const eprint = default_printer.eprint;
+/**
+ * Print a format string to process.stderr
+ * and append a newline.
+ * 
+ * @param format_string String used for formatting
+ * @param params Parameters to be inserted into the format string
+ */
+export const eprintln = default_printer.eprintln;
